96ffccfd8a85f9a8d48ea85a446906e922446c4a
Anton Pogonets <apogonets@readdle.com>
2017-03-31 20:26:46 +0300

    add missing file

---
+++ provision/130_clean.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source .profile.tmp
+
+pushd $SWIFT_SOURCE
+	ls | grep -v '^build$'| xargs rm -rf
+
+	# pushd build/Ninja-ReleaseAssert
+	# 	ls | grep -v '^swift-linux-x86_64$' | xargs rm -rf
+	# popd
+popd
+
+rm -rf $LIBICONV_ANDROID
+rm -rf $ANDROID_STANDALONE_TOOLCHAIN
+rm -rf /usr/local/lib/swift
+
+
+echo "export ANDROID_NDK=\"$ANDROID_NDK\"" >> .profile
+echo "export SWIFT_HOME=\"$SWIFT_HOME\"" >> .profile
+echo "export SWIFT_LIB=\"$SWIFT_LIB\"" >> .profile
+
+echo 'export PATH="$SWIFT_HOME/bin:$ANDROID_NDK:$PATH"' >> .profile
+
+rm .profile.tmp
