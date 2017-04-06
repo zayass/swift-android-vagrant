037c215fe5afde919663bde17b0a0e36dca3d54b
Anton Pogonets <apogonets@readdle.com>
2017-04-06 17:46:36 +0300

    fix swift cloning

--- provision/040_clone_swift.sh
+++ provision/040_clone_swift.sh
@@ -7,9 +7,16 @@ TAG="swift-3.1-RELEASE"
 mkdir -p swift-source/swift
 git clone https://github.com/apple/swift.git swift-source/swift
 
-pushd swift-source
-	swift/utils/update-checkout --clone
-	swift/utils/update-checkout --tag $TAG
+pushd swift-source/swift
+	git checkout $TAG
+
+	utils/update-checkout --clone
+	# remove generated python bytcode from outdated sources
+	find . -name '*.pyc' -exec rm {} \;
+
+	utils/update-checkout --tag $TAG
+	# remove generated python bytcode from outdated sources
+	find . -name '*.pyc' -exec rm {} \;
 popd
 
 echo 'export SWIFT_SOURCE="'`realpath ./swift-source`'"' >> .profile.tmp
